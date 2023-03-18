28a1d46091e7ab56e8054692ec1996219343a86c
Thallium <128066196+ThalliumFrog@users.noreply.github.com>
2023-03-17 19:31:56 -0500

    Update games.js

--- games.js
+++ games.js
@@ -1,28 +1,17 @@
-// Select the elements
 const gamesContainer = document.querySelector('.games');
-const searchBar = document.querySelector('.searchbar');
 const gameContainer = document.querySelector('.gamecontainer');
 const gameFrame = gameContainer.querySelector('.frame');
-const gameNav = gameContainer.querySelector('.nav');
-
-// Fetch the games data from a JSON file
 fetch('https://3kh0.github.io/assets/json/games.json')
   .then((res) => res.json())
   .then((games) => {
-    // Loop through each game and create a new game element for it
     games.forEach((game) => {
       const gameEl = document.createElement('div');
       gameEl.className = 'game';
       gameEl.innerHTML = `<img src="${"https://raw.githack.com/3kh0/3kh0-assets/main/" + game.root + "/" + game.img}" onerror="this.src='https://3kh0.github.io/assets/globe.svg'"/><span>${game.name}</span>`;
       gamesContainer.appendChild(gameEl);
-
-      // Add click event listener to the game element to show the game in the game container
       gameEl.onclick = (e) => {
         gamesContainer.classList.add('hidden');
-        searchBar.classList.add('hidden');
         gameContainer.classList.remove('hidden');
-        document.querySelector('.saveItems').classList.add('hidden');
-        document.querySelector('.navbar').classList.add('noshadow');
         gameFrame.querySelector('iframe').src = `https://raw.githack.com/3kh0/3kh0-assets/main/game?game=${game.root}`;
         gameNav.querySelector('span').textContent = game.name;
       };
