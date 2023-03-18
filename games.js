8fd325ce0190dcb52c92badf4f9111e42b3b3e50
Thallium <128066196+ThalliumFrog@users.noreply.github.com>
2023-03-17 19:38:53 -0500

    Update games.js

--- games.js
+++ games.js
@@ -1,13 +1,13 @@
 const gamesContainer = document.querySelector('.games');
 const gameContainer = document.querySelector('.gamecontainer');
 const gameFrame = gameContainer.querySelector('.frame');
-fetch('https://3kh0.github.io/assets/json/games.json')
+fetch('https://raw.githubusercontent.com/3kh0/3kh0.github.io/main/assets/json/games.json')
   .then((res) => res.json())
   .then((games) => {
     games.forEach((game) => {
       const gameEl = document.createElement('div');
       gameEl.className = 'game';
-      gameEl.innerHTML = `<img src="${"https://raw.githack.com/3kh0/3kh0-assets/main/" + game.root + "/" + game.img}" onerror="this.src='https://3kh0.github.io/assets/globe.svg'"/><span>${game.name}</span>`;
+      gameEl.innerHTML = `<img src="${"https://raw.githack.com/3kh0/3kh0-assets/main/" + game.root + "/" + game.img}" onerror="this.src='https://raw.githubusercontent.com/3kh0/3kh0.github.io/main/assets/globe.svg'"/><span>${game.name}</span>`;
       gamesContainer.appendChild(gameEl);
       gameEl.onclick = (e) => {
         gamesContainer.classList.add('hidden');
